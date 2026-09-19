c3fededd4b1d15e8d4be8928093b66237ed2766c
BuildWith-Niloy <niloykishorbhowmick@gmail.com>
2026-09-19 19:44:28 +0600

    UVA 10970 solution

---
+++ UVA/11172-Relational-Operator/solution.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>> t;
+    for(int i=0;i<t;i++)
+    {
+        long long x,y;
+
+        cin >> x >> y;
+        if(x>y)
+        {
+            cout<< ">" << endl;
+
+        }
+        else if(x<y)
+        {
+            cout<< "<" <<endl;
+        }
+        else
+            cout<< "="<< endl;
+
+    }
+    return 0;
+}
